081c42a27fa6e6208fcb4f3aeadfe4eb8ccc796f
Shun Liu <Shun.Liu@noaa.gov>
2021-08-31 14:35:22 +0000

    cvt.sh

--- monitor/cvt.sh
+++ monitor/cvt.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 
-files=`/usr/bin/ls -1`
+#files=`/usr/bin/ls -1`
+files='increments_rdas_conus.py'
 for  file in $files
 do
-#sed 's/emc.campara/wx22hl/g' $file > $file.new
-#mv $file.new $file
 echo $file
+
+sed 's/rdas/rdasConus/' $file > $file.new
+mv $file.new $file
+
+sed 's/fv3lamda/rdas/' $file > $file.new
+mv $file.new $file
+
+sed 's/fv3lam/rrfs/' $file > $file.new
+mv $file.new $file
+
 done
